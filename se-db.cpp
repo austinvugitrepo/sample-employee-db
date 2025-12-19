9b3c5bf956e1ae22f3d6e7bf8d1facdcb8469c5a
Austin <freeluck.v.i.p999@gmail.com>
2025-12-19 13:41:04 -0600

    finished name checker via token and handled error with try/catch

--- se-db.cpp
+++ se-db.cpp
@@ -17,14 +17,24 @@ int main(){
   strncpy(cidcopy, id.c_str(), sizeof(cidcopy) - 1);
   cidcopy[sizeof(cidcopy) - 1] = '\0';
   char* token = strtok(cidcopy, " ");
-  
+ // count token 
   while (token != NULL) {
      tokcount++;
      token = strtok(NULL, " ");
   }
-  
+
+  int idnumver;
+  //converting id to integer
+  try { //try catch combo helps with not getting core dumped
+     idnumver = std::stoi(id);
+  } catch (const std::invalid_argument& error){
+    //error is variable, .what shows exact error message
+    std::cerr << "Error " << error.what() << std::endl;
+    return 1;
+  }
+  //token checker for " "
   if (tokcount == 1){
-    std::cout << "Your employee ID is " << id << std::endl; //remove later
+    std::cout << "Your employee ID is " << idnumver << std::endl; //remove later
   } else {
     std::cout << "Error: Could not process ID, try again.\n";
   }
