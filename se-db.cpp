70c805d40469f5c13b3f5a0e4c8315a5b199d84c
Austin <freeluck.v.i.p999@gmail.com>
2025-12-19 14:16:37 -0600

    added email, and year. for year added sucessful (in my opinion) 4 digit year checker

--- se-db.cpp
+++ se-db.cpp
@@ -29,7 +29,7 @@ int main(){
      idnumver = std::stoi(id);
   } catch (const std::invalid_argument& error){
     //error is variable, .what shows exact error message
-    std::cerr << "Error " << error.what() << std::endl;
+    std::cerr << "Error: NOT A VALID NUMBER, try again: " << error.what() << std::endl;
     return 1;
   }
   //token checker for " "
@@ -38,6 +38,33 @@ int main(){
   } else {
     std::cout << "Error: Could not process ID, try again.\n";
   }
+  
+  std::string email;
+  std::cout << "Enter your email:\n";
+  std::getline(std::cin, email);
+  std::cout << "Your email is " << email << std::endl; //remove later
+                                                       
+  std::string year;
+  std::cout << "Enter your year of birth:\n";
+  std::getline(std::cin, year);
+  //4 digit checker begins here
+  if (year.length() == 4){
+    std::cout << "4 character response detected\n";
+  } else {
+     std::cerr << "Error: NOT A VALID 4 DIGIT NUMBER, try again:\n";
+     return 1;
+  }
+  int yearnum;
+  //converting id to integer
+  try { //try catch combo helps with not getting core dumped
+     yearnum = std::stoi(year);
+  } catch (const std::invalid_argument& error){
+    //error is variable, .what shows exact error message
+    std::cerr << "Error: NOT A VALID 4 DIGIT NUMBER, try again: " << error.what() << std::endl;
+    return 1;
+  }
+  std::cout << "Your birth year is " << yearnum << std::endl; //remove later
+
   
   
   return 0;
