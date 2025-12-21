74f99c0d246753926f660006dbb8e32065994b3b
Your Name <freeluck.v.i.p999@gmail.com>
2025-12-21 06:27:02 +0000

    finished pointers for my variables (i think) and switched . for -> and got rid of extra * to satisfy compiler, so new changes mean dereferencing pointer and accessing the value

--- se-db.cpp
+++ se-db.cpp
@@ -19,13 +19,13 @@ int main(){
 
   std::cout << "Enter your name:\n";
   std::getline(std::cin, *nameptr);
-  std::cout << "Your name is " << emp.name << std::endl; //remove later
+  std::cout << "Your name is " << *nameptr << std::endl; //remove later
   std::cout << "Enter your employee ID:\n";
   std::getline(std::cin, *idptr);
   int tokcount = 0;
   //creating token
   char cidcopy[500];
-  strncpy(cidcopy, emp.id.c_str(), sizeof(cidcopy) - 1);
+  strncpy(cidcopy, idptr->c_str(), sizeof(cidcopy) - 1);
   cidcopy[sizeof(cidcopy) - 1] = '\0';
   char* token = strtok(cidcopy, " ");
  // count token 
@@ -37,7 +37,7 @@ int main(){
   int idnumver;
   //converting id to integer
   try { //try catch combo helps with not getting core dumped
-     idnumver = std::stoi(emp.id);
+     idnumver = std::stoi(*idptr);
   } catch (const std::invalid_argument& error){
     //error is variable, .what shows exact error message
     std::cerr << "Error: NOT A VALID NUMBER, try again: " << error.what() << std::endl;
@@ -52,12 +52,12 @@ int main(){
   
   std::cout << "Enter your email:\n";
   std::getline(std::cin, *emailptr);
-  std::cout << "Your email is " << emp.email << std::endl; //remove later
+  std::cout << "Your email is " << *emailptr << std::endl; //remove later
                                                        
   std::cout << "Enter your year of birth:\n";
   std::getline(std::cin, *yearptr);
   //4 digit checker begins here
-  if (emp.year.length() == 4){
+  if (yearptr->length() == 4){
     std::cout << "4 character response detected\n";
   } else {
      std::cerr << "Error: NOT A VALID 4 DIGIT NUMBER, try again:\n";
@@ -66,7 +66,7 @@ int main(){
   int yearnum;
   //converting id to integer
   try { //try catch combo helps with not getting core dumped
-     yearnum = std::stoi(emp.year);
+     yearnum = std::stoi(*yearptr);
   } catch (const std::invalid_argument& error){
     //error is variable, .what shows exact error message
     std::cerr << "Error: NOT A VALID 4 DIGIT NUMBER, try again: " << error.what() << std::endl;
