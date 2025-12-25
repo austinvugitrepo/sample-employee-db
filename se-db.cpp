f9af39a7e9217b87141d8ab211de2c7960cb6054
Your Name <freeluck.v.i.p999@gmail.com>
2025-12-25 23:27:07 +0000

    got rid of individual member pointers to instead access them directly, created empdata array for data collection, and emp for final array to mess around

--- se-db.cpp
+++ se-db.cpp
@@ -12,11 +12,8 @@ struct Employee {
 
 int main(){
   int empcounter = 0;
-  struct Employee emp;
-  std::string* nameptr = &emp.name;
-  std::string* idptr = &emp.id;
-  std::string* emailptr = &emp.email;
-  std::string* yearptr = &emp.year;
+  int MAX_EMP = 250; // max number of employees
+  struct Employee empdata[MAX_EMP]; //array for data collection
 
   std::string loop = "i";
 
@@ -45,21 +42,34 @@ while (loop != "q") {
     std::cout << "Exited Sucessfully.\n";
     } else if (empcounter > 0){
       std::cout << "Attempting to Register "<< empcounter << " Employees..." << std::endl;
+      struct Employee* emp = new Employee[empcounter]; // allocating memory for finished array of all preregistered employees
+      for(int i = 0; i < empcounter; i++){
+        emp[i] = empdata[i];           // moving data collection array to final array
+      }
+      std::cout << "Employess being sorted...\n"; // print for-loop debug section
+      for(int i = 0; i < empcounter; i++){
+        std::cout << "---------------------\n";
+        std::cout << emp[i].name << std::endl;
+        std::cout << emp[i].id << std::endl;
+        std::cout << emp[i].email << std::endl;
+        std::cout << emp[i].year << std::endl;
+      }
+      delete[] emp; // free memory 
       std::cout << "Exited Sucessfully.\n";
     }
     break;
   } else {
      
      std::cout << "Enter your name: ";
-     std::getline(std::cin, *nameptr);
+     std::getline(std::cin, empdata[empcounter].name);
      std::cout << "\033[A\33[2K\r" << std::flush; //clear user input on previous line so user does not see their input
      std::cout << "Enter your employee ID: ";
-     std::getline(std::cin, *idptr);
+     std::getline(std::cin, empdata[empcounter].id);
      std::cout << "\033[A\33[2K\r" << std::flush; //clear user input on previous line so user does not see their input
     int tokcount = 0;
     //creating token
     char cidcopy[500];
-    strncpy(cidcopy, idptr->c_str(), sizeof(cidcopy) - 1);
+    strncpy(cidcopy, empdata[empcounter].id.c_str(), sizeof(cidcopy) - 1);
     cidcopy[sizeof(cidcopy) - 1] = '\0';
     char* token = strtok(cidcopy, " ");
     // count token 
@@ -70,7 +80,7 @@ while (loop != "q") {
 
    //converting id to integer
     try { //try catch combo helps with not getting core dumped
-    std::stoi(*idptr);
+    std::stoi(empdata[empcounter].id);
    } catch (const std::invalid_argument& error){
     //error is variable, .what shows exact error message
     std::cout << std::string(61, '*') << std::endl;
@@ -90,13 +100,13 @@ while (loop != "q") {
   }
   
   std::cout << "Enter your email: ";
-  std::getline(std::cin, *emailptr);
+  std::getline(std::cin, empdata[empcounter].email);
   std::cout << "\033[A\33[2K\r" << std::flush; //clear user input on previous line so user does not see their input
   std::cout << "Enter your year of birth: ";
-  std::getline(std::cin, *yearptr);
+  std::getline(std::cin, empdata[empcounter].year);
   std::cout << "\033[A\33[2K\r" << std::flush; //clear user input on previous line so user does not see their input
   //4 digit checker begins here
-  if (yearptr->length() == 4){
+  if (empdata[empcounter].year.length() == 4){
   } else {
     std::cout << std::string(48, '*') << std::endl;
     std::cerr << "/ \033[31mERROR: NOT A VALID 4 DIGIT NUMBER, try again.\033[0m/\n";
@@ -106,7 +116,7 @@ while (loop != "q") {
   }
   //converting id to integer
   try { //try catch combo helps with not getting core dumped
-    std::stoi(*yearptr);
+    std::stoi(empdata[empcounter].year);
   } catch (const std::invalid_argument& error){
     //error is variable, .what shows exact error message
     std::cout << std::string(69, '*') << std::endl;
