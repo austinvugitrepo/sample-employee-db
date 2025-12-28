5d5a906f2ce5a0ca47795b17a24a36e152a00b0b
Your Name <freeluck.v.i.p999@gmail.com>
2025-12-28 19:23:13 +0000

    fixed: bubble sorts all digits not just one anymore. todo: still have to fix string bypassing stoi, lastly combined error checking, might turn it into a helper function

--- se-db.cpp
+++ se-db.cpp
@@ -5,16 +5,17 @@
 #include <thread> // for sleep
 struct Employee {
   std::string name;
-  std::string id;
+  int id;
   std::string email;
-  std::string year;
+  int year;
 };
 
 int main(){
   int empcounter = 0;
-  int MAX_EMP = 250; // max number of employees
+  int MAX_EMP = 999; // max number of employees
   struct Employee empdata[MAX_EMP]; //array for data collection
-
+  std::string uid; // for emp id
+  std::string uyear; // for emp year
   std::string loop = "i";
 
 while (loop != "q") {
@@ -75,12 +76,22 @@ while (loop != "q") {
      std::getline(std::cin, empdata[empcounter].name);
      std::cout << "\033[A\33[2K\r" << std::flush; //clear user input on previous line so user does not see their input
      std::cout << "Enter your employee ID: ";
-     std::getline(std::cin, empdata[empcounter].id);
+     std::getline(std::cin, uid);
      std::cout << "\033[A\33[2K\r" << std::flush; //clear user input on previous line so user does not see their input
-    int tokcount = 0;
+     //3 digit checker begins here
+     if (uid.length() == 3){
+    } else {
+    std::cout << std::string(48, '*') << std::endl;
+    std::cerr << "/ \033[31mERROR: NOT A VALID 3 DIGIT EMPLOYEE ID, try again.\033[0m/\n";
+    std::cout << std::string(48, '*') << std::endl;
+    std::this_thread::sleep_for(std::chrono::milliseconds(550)); //main thread program sleeps .550 seconds
+    continue;
+    }
+     
     //creating token
+    int tokcount = 0;
     char cidcopy[500];
-    strncpy(cidcopy, empdata[empcounter].id.c_str(), sizeof(cidcopy) - 1);
+    strncpy(cidcopy, uid.c_str(), sizeof(cidcopy) - 1);
     cidcopy[sizeof(cidcopy) - 1] = '\0';
     char* token = strtok(cidcopy, " ");
     // count token 
@@ -89,9 +100,20 @@ while (loop != "q") {
      token = strtok(NULL, " ");
     }
 
+    //token checker for " "
+   if (tokcount == 1){
+   } else {
+    std::cout << std::string(43, '*') << std::endl;
+    std::cout << "/ \033[31mERROR: Could not process ID, try again.\033[0m /\n";
+    std::cout << std::string(43, '*') << std::endl;
+    std::this_thread::sleep_for(std::chrono::milliseconds(550)); //main thread program sleeps .550 seconds
+    continue;
+   }
+  
+
    //converting id to integer
     try { //try catch combo helps with not getting core dumped
-    std::stoi(empdata[empcounter].id);
+   empdata[empcounter].id = std::stoi(uid);
    } catch (const std::invalid_argument& error){
     //error is variable, .what shows exact error message
     std::cout << std::string(61, '*') << std::endl;
@@ -100,24 +122,15 @@ while (loop != "q") {
     std::this_thread::sleep_for(std::chrono::milliseconds(550)); //main thread program sleeps .550 seconds
     continue;
    }
-  //token checker for " "
-  if (tokcount == 1){
-  } else {
-    std::cout << std::string(43, '*') << std::endl;
-    std::cout << "/ \033[31mERROR: Could not process ID, try again.\033[0m /\n";
-    std::cout << std::string(43, '*') << std::endl;
-    std::this_thread::sleep_for(std::chrono::milliseconds(550)); //main thread program sleeps .550 seconds
-    continue;
-  }
-  
+ 
   std::cout << "Enter your email: ";
   std::getline(std::cin, empdata[empcounter].email);
   std::cout << "\033[A\33[2K\r" << std::flush; //clear user input on previous line so user does not see their input
   std::cout << "Enter your year of birth: ";
-  std::getline(std::cin, empdata[empcounter].year);
+  std::getline(std::cin, uyear);
   std::cout << "\033[A\33[2K\r" << std::flush; //clear user input on previous line so user does not see their input
   //4 digit checker begins here
-  if (empdata[empcounter].year.length() == 4){
+  if (uyear.length() == 4){
   } else {
     std::cout << std::string(48, '*') << std::endl;
     std::cerr << "/ \033[31mERROR: NOT A VALID 4 DIGIT NUMBER, try again.\033[0m/\n";
@@ -125,9 +138,32 @@ while (loop != "q") {
     std::this_thread::sleep_for(std::chrono::milliseconds(550)); //main thread program sleeps .550 seconds
     continue;
   }
+
+   //creating token
+    int ytokcount = 0;
+    char ycidcopy[500];
+    strncpy(ycidcopy, uyear.c_str(), sizeof(ycidcopy) - 1);
+    ycidcopy[sizeof(ycidcopy) - 1] = '\0';
+    char* ytoken = strtok(ycidcopy, " ");
+    // count token 
+    while (ytoken != NULL) {
+     ytokcount++;
+     ytoken = strtok(NULL, " ");
+    }
+
+    //token checker for " "
+   if (ytokcount == 1){
+   } else {
+    std::cout << std::string(43, '*') << std::endl;
+    std::cout << "/ \033[31mERROR: Could not process ID, try again.\033[0m /\n";
+    std::cout << std::string(43, '*') << std::endl;
+    std::this_thread::sleep_for(std::chrono::milliseconds(550)); //main thread program sleeps .550 seconds
+    continue;
+   }
+  
   //converting id to integer
   try { //try catch combo helps with not getting core dumped
-    std::stoi(empdata[empcounter].year);
+   empdata[empcounter].year = std::stoi(uyear);
   } catch (const std::invalid_argument& error){
     //error is variable, .what shows exact error message
     std::cout << std::string(69, '*') << std::endl;
